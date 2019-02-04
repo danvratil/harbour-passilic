1a751f747063e99de40828b428ce26f5aaa102df
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2019-02-04 13:31:35 +0800

    Create harbour-passilic-zh_cn.ts

---
+++ translations/harbour-passilic-zh_cn.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>My Cover</source>
+        <translation>我的封面</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>Show Page 2</source>
+        <translation>显示第二页</translation>
+    </message>
+    <message>
+        <source>UI Template</source>
+        <translation>用户界面模板</translation>
+    </message>
+    <message>
+        <source>Hello Sailors</source>
+        <translation>你好，水手</translation>
+    </message>
+</context>
+<context>
+    <name>SecondPage</name>
+    <message>
+        <source>Nested Page</source>
+        <translation>嵌套页</translation>
+    </message>
+    <message>
+        <source>Item</source>
+        <translation>项目</translation>
+    </message>
+</context>
+</TS>
+© 2019 GitHub, Inc.
