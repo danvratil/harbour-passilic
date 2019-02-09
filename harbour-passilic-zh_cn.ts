5922ebabb69d5cac94e5446ab1d58e479be80777
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2019-02-09 10:15:06 +0800

    Create harbour-passilic-zh_cn.ts

---
+++ harbour-passilic-zh_cn.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>PasswordProvider</name>
+    <message>
+        <source>Failed to decrypt password: GPG is not available</source>
+        <translation type="解密密码失败: GPG不可用"></translation>
+    </message>
+    <message>
+        <source>Failed to decrypt password: Failed to start GPG</source>
+        <translation type="解密密码失败: 开启GPG失败"></translation>
+    </message>
+    <message>
+        <source>Failed to decrypt password</source>
+        <translation type="解密密码失败"></translation>
+    </message>
+    <message>
+        <source>Failed to decrypt password: %1</source>
+        <translation type="解密密码失败: %1"></translation>
+    </message>
+    <message>
+        <source>Cancelled by user.</source>
+        <translation type="已被用户取消"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>Passilic</source>
+        <translation type="Passilic"></translation>
+    </message>
+    <message>
+        <source>Daniel Vrátil</source>
+        <translation type="">Daniel Vrátil</translation>
+    </message>
+</context>
+</TS>
