4d4334982fd1d89564b5d286c1d405e1392f4de8
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2019-02-15 22:10:18 +0800

    Rename harbour-passilic-zh_cn.ts to harbour-passilic-zh.ts

---
+++ translations/harbour-passilic-zh.ts
@@ -0,0 +1,114 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1"> language="zh_cn">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <source>Passilic</source>
+        <translation>Passilic</translation>
+    </message>
+    <message>
+        <source>Version %1</source>
+        <translation>版本 %1</translation>
+    </message>
+    <message>
+        <source>Author: Daniel Vrátil</source>
+        <translation>软件开发者: Daniel Vrátil</translation>
+    </message>
+    <message>
+        <source>Homepage: &lt;a href=&quot;%1&quot;&gt;Github&lt;/a&gt;</source>
+        <translation>主页: &lt;a href=&quot;%1&quot;&gt;Github&lt;/a&gt;</translation>
+    </message>
+    <message>
+        <source>License: GPLv3</source>
+        <translation>许可证: GPLv3</translation>
+    </message>
+</context>
+<context>
+    <name>GlobalPullDownMenu</name>
+    <message>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>搜索</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>设置</translation>
+    </message>
+</context>
+<context>
+    <name>PasswordDelegate</name>
+    <message>
+        <source>Password copied to clipboard</source>
+        <translation >密码已复制到剪贴板</translation>
+    </message>
+    <message>
+        <source>Expire password</source>
+        <translation >密码已过期</translation>
+    </message>
+    <message>
+        <source>Password will expire</source>
+        <translation>密码将会过期</translation>
+    </message>
+</context>
+<context>
+    <name>PasswordListPage</name>
+    <message>
+        <source>Passilic</source>
+        <translation>Passilic</translation>
+    </message>
+</context>
+<context>
+    <name>PasswordProvider</name>
+    <message>
+        <source>Failed to decrypt password: GPG is not available</source>
+        <translation>解密密码失败: GPG不可用</translation>
+    </message>
+    <message>
+        <source>Failed to decrypt password: Failed to start GPG</source>
+        <translation>解密密码失败: 未能GPG</translation>
+    </message>
+    <message>
+        <source>Failed to decrypt password</source>
+        <translation>解密密码失败</translation>
+    </message>
+    <message>
+        <source>Failed to decrypt password: %1</source>
+        <translation>解密密码失败: %1</translation>
+    </message>
+    <message>
+        <source>Cancelled by user.</source>
+        <translation>已被用戶取消</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>Passilic</source>
+        <translation>Passilic</translation>
+    </message>
+    <message>
+        <source>Daniel Vrátil</source>
+        <translation>Daniel Vrátil</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Password expiration</source>
+        <translation>密码过期</translation>
+    </message>
+    <message>
+        <source>%1 s</source>
+        <extracomment>number of seconds</extracomment>
+        <translation>%1 秒</translation>
+    </message>
+</context>
+</TS
