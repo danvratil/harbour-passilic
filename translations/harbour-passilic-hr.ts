b28e04fd8dafa774e855921624b0f9799a0f292c
milotype <43657314+milotype@users.noreply.github.com>
2020-01-22 20:49:16 +0100

    Added harbour-passilic-hr.ts
    Initial release of croatian translation

---
+++ translations/harbour-passilic-hr.ts
@@ -0,0 +1,148 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hr">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About</source>
+        <translation>Informacije</translation>
+    </message>
+    <message>
+        <source>Passilic</source>
+        <translation>Passilic</translation>
+    </message>
+    <message>
+        <source>Version %1</source>
+        <translation>Verzija %1</translation>
+    </message>
+    <message>
+        <source>Author: Daniel Vrátil</source>
+        <translation>Autor: Daniel Vrátil</translation>
+    </message>
+    <message>
+        <source>Homepage: &lt;a href=&quot;%1&quot;&gt;Github&lt;/a&gt;</source>
+        <translation>Web-stranica: &lt;a href=&quot;%1&quot;&gt;Github&lt;/a&gt;</translation>
+    </message>
+    <message>
+        <source>License: GPLv3</source>
+        <translation>Licenca: GPLv3</translation>
+    </message>
+</context>
+<context>
+    <name>GeneratePasswordDialog</name>
+    <message>
+        <source>Password Length</source>
+        <translation>Duljina lozinke</translation>
+    </message>
+    <message>
+        <source>Allow non-alphanumeric characters</source>
+        <translation>Dozvoli ne-alfanumeričke znakove</translation>
+    </message>
+</context>
+<context>
+    <name>GlobalPullDownMenu</name>
+    <message>
+        <source>About</source>
+        <translation>Informacije</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>Traži</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Postavke</translation>
+    </message>
+    <message>
+        <source>New Password</source>
+        <translation>Nova lozinka</translation>
+    </message>
+</context>
+<context>
+    <name>NewPasswordDialog</name>
+    <message>
+        <source>Name:</source>
+        <translation>Ime:</translation>
+    </message>
+    <message>
+        <source>Password:</source>
+        <translation>Lozinka:</translation>
+    </message>
+    <message>
+        <source>Additional Info:</source>
+        <translation>Dodatne informacije:</translation>
+    </message>
+    <message>
+        <source>Generate Password</source>
+        <translation>Generiraj lozinku</translation>
+    </message>
+</context>
+<context>
+    <name>PasswordDelegate</name>
+    <message>
+        <source>Password copied to clipboard</source>
+        <translation>Lozinka je kopirana u međuspremnik</translation>
+    </message>
+    <message>
+        <source>Expire password</source>
+        <translation>Istek lozinke</translation>
+    </message>
+    <message>
+        <source>Password will expire</source>
+        <translation>Lozinka će isteći</translation>
+    </message>
+</context>
+<context>
+    <name>PasswordListPage</name>
+    <message>
+        <source>Passilic</source>
+        <translation>Passilic</translation>
+    </message>
+</context>
+<context>
+    <name>PasswordProvider</name>
+    <message>
+        <source>Failed to decrypt password: GPG is not available</source>
+        <translation>Neuspjelo dešifriranje lozinke: GPG nije dostupan</translation>
+    </message>
+    <message>
+        <source>Failed to decrypt password: Failed to start GPG</source>
+        <translation>Neuspjelo dešifriranje lozinke: Neuspjelo pokretanje GPG-a</translation>
+    </message>
+    <message>
+        <source>Failed to decrypt password</source>
+        <translation>Neuspjelo dešifriranje lozinke</translation>
+    </message>
+    <message>
+        <source>Failed to decrypt password: %1</source>
+        <translation>Neuspjelo dešifriranje lozinke: %1</translation>
+    </message>
+    <message>
+        <source>Cancelled by user.</source>
+        <translation>Prekinuto od korisnika.</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>Passilic</source>
+        <translation>Passilic</translation>
+    </message>
+    <message>
+        <source>Daniel Vrátil</source>
+        <translation>Daniel Vrátil</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Password expiration</source>
+        <translation>Istek lozinke</translation>
+    </message>
+    <message>
+        <source>%1 s</source>
+        <extracomment>number of seconds</extracomment>
+        <translation>%1 s</translation>
+    </message>
+</context>
+</TS>
